86665944e3530fb71a80d229acddf6e462ac4c47
Samuel Newport <samuelsn6399@yahoo.com>
2023-03-04 22:27:39 -0600

    Added new user feature (IN PROGRESS)

--- myRunnningLog/Program.cs
+++ myRunnningLog/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MyRunningLog
 {
@@ -7,23 +11,47 @@ namespace MyRunningLog
     {
         static void Main(string[] args)
         {
-            // Create Variables
+                                                                                                                                // Create Variables
             double totalMiles = 0.0;
-            TimeSpan totalTime = new TimeSpan(0, 0, 0); // Cool object that allows me to log the user input time as h, m, s
-            double weeklyGoal = 10.0;   // Change this to a user input when a user creates their profile
-            
-            // Master loop - Run through data entry prompt until user says to stop
-            while (true)
-            {
-                Console.WriteLine("Log Run (type 'q' to quit): ");
-                Console.WriteLine("Log Mileage (ex: 2.56): ");
-                string inputMiles = Console.ReadLine();
+            TimeSpan totalTime = new TimeSpan(0, 0, 0);                                                                         // Cool object that allows me to log the user input time as h, m, s
+            double weeklyGoal = 10.0;                                                                                           // Change this to a user input when a user creates their profile
+            int numberOfUsers = 0;
 
-                // If the user inputs the quit command ('q') then break the master while loop
-                if(inputMiles.ToLower() == "q")
+            var Users = new List<string> {};
+
+            while (true)                                                                                                        // Master loop - Run through data entry prompt until user says to stop
+            {
+                Console.WriteLine("Select a user (Enter an integer) or press q to quit: ");                                     // Prompt for the user's profile, plus have an option to choose new user and create a profile
+                Users.ForEach((v) => Console.WriteLine($"{v + 1}: {0}", v));
+                Console.WriteLine($"{numberOfUsers+1}: New User");
+                string inputUserNum = Console.ReadLine();
+
+                if (inputUserNum.ToLower() == "q")                                                                              // If the user inputs the quit command ('q') then break the master while loop
                 {
                     break;
                 }
+
+                int userNum;
+                if(!int.TryParse(inputUserNum, out userNum))
+                {
+                    Console.WriteLine("Invalid input. Enter an integer");
+                    continue;
+                }
+                if(userNum > numberOfUsers+1 || userNum < 1)
+                {
+                    Console.WriteLine("Invalid input. Enter an integer from the selection provided");
+                    continue;
+                }
+
+                if(userNum == numberOfUsers)
+                {
+                    // Go to newUser Class
+                    numberOfUsers++;
+                }
+
+                Console.WriteLine("Log a Run: ");
+                Console.WriteLine("Enter Mileage (ex: 2.56): ");
+                string inputMiles = Console.ReadLine();
 
                 // Check if the user inputs a valid number of miles
                 double miles;
@@ -36,7 +64,7 @@ namespace MyRunningLog
                 }
 
                 // Prompt user for the duration of their run
-                Console.WriteLine("Log time ('hh:mm:ss'): ");
+                Console.WriteLine("Enter Time ('hh:mm:ss'): ");
                 string inputTime = Console.ReadLine();
                 TimeSpan time;
                 // Convert the string inputTime into a time object called time
